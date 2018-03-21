fe70325f2e3674d3bb98017a17a9fe5d8fb4cae4
Alexis Bugielski <alexis.bugielski@gmail.com>
2018-03-21 12:17:35 -0400

    add of the email regex

--- Lab. 3/validate_email_with_regex.php
+++ Lab. 3/validate_email_with_regex.php
@@ -1,5 +1,16 @@
 <?php
 
 // Find regular expression online to match any e-mail address.
+$exp = "/^[\w.-]+@[\w.-]+\.[a-z]{2,6}$/";
 // Use it to validate an array of 5 strings.
+$array = array("toto.titi@tata.com", "toto.titi.tata@o.net", "toto.titi.tata@onetor", "totoo.netor@", "toto.titi.tata@net");
 // Make 3 of these 5 strings invalid email adresses (e.g. missing TLD, missing @, illegal characters, etc...)
+foreach($array as $email){
+	if(preg_match($exp,$email)){
+		echo(preg_match_all($exp,$email));
+		echo ("$email is valid email <br>");
+	}
+	else{
+		echo ("$email is not valid email <br>");
+	}
+}
