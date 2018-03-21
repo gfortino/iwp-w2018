b30c97f00315f23b6b0817530de8336597a830a6
Giuseppe Fortino <Giuseppe.Fortino@gmail.com>
2018-03-21 11:43:24 -0400

    Added details for Lab. 3

--- Lab. 3/list_content_directory.php
+++ Lab. 3/list_content_directory.php
@@ -1,3 +1,5 @@
 <?php
 
-// Print this project directory's file listing to screen
+// Print this project directory's file listing to screen.
+// If you find a directory, enter it recursively.
+// Using a function can be useful for recursion.
