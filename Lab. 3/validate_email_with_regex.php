bbc6ab119943f4bec87b129e1e67f0631ea1a7a7
TomK <tomklotz.pro@gmail.com>
2018-03-21 22:04:06 -0400

    correction of branch master

---
+++ Lab. 3/validate_email_with_regex.php
@@ -0,0 +1,4 @@
+<?php
+// Find regular expression online to match any e-mail address.
+// Use it to validate an array of 5 strings.
+// Make 3 of these 5 strings invalid email adresses (e.g. missing TLD, missing @, illegal characters, etc...)
