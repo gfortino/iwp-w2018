10b80138e36533154fc5829178cc862479ea3d2d
TomK <tomklotz.pro@gmail.com>
2018-03-21 21:54:52 -0400

    Revert "Added details for Lab. 3"
    This reverts commit b30c97f00315f23b6b0817530de8336597a830a6.

--- Lab. 3/list_content_directory.php
+++ Lab. 3/list_content_directory.php
@@ -1,5 +1,3 @@
 <?php
 
-// Print this project directory's file listing to screen.
-// If you find a directory, enter it recursively.
-// Using a function can be useful for recursion.
+// Print this project directory's file listing to screen
