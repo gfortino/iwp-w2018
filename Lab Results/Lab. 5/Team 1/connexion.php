d77c2b7bc2c823eda6561be6076379260a12d01f
ZheruiL <zherui.li@outlook.com>
2018-04-13 21:29:41 -0400

    team1lab5

---
+++ Lab Results/Lab. 5/Team 1/connexion.php
@@ -0,0 +1,23 @@
+<?php
+	//mysql_connect(infodb.iutmetz.univ-lorraine.fr/phpmyadmin,li128u_appli,31521728);
+	$db_config=array();
+	$db_config['SGBD']='mysql';
+	$db_config['HOST']='127.0.0.1';
+	$db_config['DB_NAME']='lab5';//the name of the base le nom de base
+	$db_config['USER']='root';
+	$db_config['PASSWORD']='';
+	
+	//====
+	//connection avec PDO
+	try
+	{
+
+	$objPdo=new PDO($db_config['SGBD'].':host='.$db_config['HOST'].';dbname='.$db_config['DB_NAME'],$db_config['USER'],$db_config['PASSWORD']);
+	unset($db_config);
+
+	}
+	catch(Exception $exception)
+	{
+		die($exception->getMessage());
+	}
+?>
---
+++ Lab Results/Lab. 5/Team 1/signup.php
@@ -0,0 +1,112 @@
+<?php
+	include "connexion.php";
+    
+    if(isset($_POST["signup"])){
+		$login=$_POST['login'];
+		$password=$_POST['password'];
+		$password=md5($password);
+		$lastname=$_POST['lastname'];
+
+		$objPdo->query("INSERT INTO user (rowid, login, password, lastname) VALUES (NULL, '".$login."', '".$password."', '".$lastname."');");
+		header ("location:index.php");
+    }
+?>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<style>
+.form-signin
+{
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.form-signin .form-signin-heading, .form-signin .checkbox
+{
+    margin-bottom: 10px;
+}
+.form-signin .checkbox
+{
+    font-weight: normal;
+}
+.form-signin .form-control
+{
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.form-signin .form-control:focus
+{
+    z-index: 2;
+}
+.form-signin input[type="text"]
+{
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.form-signin input[type="password"]
+{
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.account-wall
+{
+    margin-top: 20px;
+    padding: 40px 0px 20px 0px;
+    background-color: #f7f7f7;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.login-title
+{
+    color: #555;
+    font-size: 18px;
+    font-weight: 400;
+    display: block;
+}
+.profile-img
+{
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+.need-help
+{
+    margin-top: 10px;
+}
+.new-account
+{
+    display: block;
+    margin-top: 10px;
+}
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">Sign up to continue</h1>
+            <div class="account-wall">
+                <form class="form-signin" action="signup.php" method="post">
+				<input name="lastname" type="text" class="form-control" placeholder="Lastname" required autofocus>
+                <input name="login" type="text" class="form-control" placeholder="Login" required autofocus>
+                <input name="password" type="password" class="form-control" placeholder="Password" required>
+                <button name="signup" class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in
+				</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+  
